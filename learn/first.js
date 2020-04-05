8506df30a4a9597905e81c4b3ec5bac56ed9e482
Dr. Swaminathan J <swaminathanj@yahoo.com>
2020-04-05 12:07:32 +0530

    Update first.js

--- learn/first.js
+++ learn/first.js
@@ -7,7 +7,7 @@ function one() {
     delem = document.getElementById("elem"); // elem may be button, ol, ul, p, div or any object
     elem.value = "New text"; // For changing button text
     elem.innerHTML = "New text"; // For changing normal text
-    elem.className = newtheme; // where css file definition includes .newtheme { color: blue; }
+    elem.className = "newtheme"; // where css file definition includes .newtheme { color: blue; }
 */
 
 count = 0;
