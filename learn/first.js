506422f4215abffba15c3685b5e32923aeccc20a
Dr. Swaminathan J <swaminathanj@yahoo.com>
2020-04-05 11:06:46 +0530

    Update first.js

--- learn/first.js
+++ learn/first.js
@@ -2,9 +2,9 @@ function one() {
   alert('You called function one');
 }
 
-count = 0;
+count = 1;
 function increment() {
   var dcounter = document.getElementById("count");
-  count++;
   dcounter.value("Clicked " + count + " times");
+  count++;
 }
