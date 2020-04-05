2ae809ccbb1ddcd37f0515dbe8908fd101504df4
Dr. Swaminathan J <swaminathanj@yahoo.com>
2020-04-05 11:53:14 +0530

    Update first.js

--- learn/first.js
+++ learn/first.js
@@ -2,10 +2,17 @@ function one() {
   alert('You called function one');
 }
 
+
+/* 
+    delem = document.getElementById("elem"); // elem may be button, ol, ul, p, div or any object
+    elem.value = "New text"; // For changing button text
+    elem.innerHTML = "New text"; // For changing normal text
+    elem.className = newtheme; // where css file definition includes .newtheme { color: blue; }
+*/
+
 count = 0;
 function increment() {
   var dcounter = document.getElementById("count");
   count++;
-  alert('counter incremented to ' + count);
   dcounter.value = "Clicked " + count + " times";
 }
